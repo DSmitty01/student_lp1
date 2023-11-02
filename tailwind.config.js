3de8e565e1773251664fde4fa41895049e3157b3
Gary <alfordg15@gmail.com>
2023-11-02 00:44:44 -0400

    responsive updates

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "siteBlue" : "#0066FF",
+      },
+    },
+  },
+  plugins: [],
+}
+
