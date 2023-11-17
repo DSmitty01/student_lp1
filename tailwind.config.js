9761627785fcaa293c6fcedb70b47f65fe2b7720
Gary <alfordg15@gmail.com>
2023-11-17 01:54:02 -0500

    site updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      screens: {
+        xs: "500",
+      },
+
       colors: {
         siteBlue: "#0066FF",
       },
